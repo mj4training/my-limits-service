449e4b3464de1cab87d4fe25957fd7e03edf336d
mj4training <mj4training@gmail.com>
2019-01-05 17:45:54 -0500

    First commit

---
+++ src/test/java/com/ribeach/spring/microservice/mylimitsservice/MyLimitsServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ribeach.spring.microservice.mylimitsservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyLimitsServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
